61b73149ec65c2a944f574d470f8926196876b75
MrKrot <vkorotanec@gmail.com>
2025-03-23 00:18:11 +0100

    added tve_engine_swap_chain

--- build/engine_device.d
+++
@@ -1,4 +0,0 @@
-build/engine_device.o: src/engine_device.cpp include/engine_device.hpp \
- include/tve_window.hpp
-include/engine_device.hpp:
-include/tve_window.hpp:
--- build/tve_window.d
+++
@@ -1,2 +0,0 @@
-build/tve_window.o: src/tve_window.cpp include/tve_window.hpp
-include/tve_window.hpp:
